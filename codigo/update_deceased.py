64576ada5b70c01d867322a32910c4d41380b5a7
Rodrigo L.C <rodrigolealc@ciencias.unam.mx>
2020-06-08 17:35:07 +0100

    new files with daily counts side by side

--- codigo/update_deceased.py
+++ codigo/update_deceased.py
@@ -9,18 +9,34 @@ dir_datos_abiertos = os.path.join(repo, 'datos_abiertos', '')
 dir_series_abiertos = os.path.join(dir_datos_abiertos, 'series_de_tiempo', 'nuevos', '')
 dir_save = os.path.join(dir_datos_abiertos, 'formato_especial', '')
 
+## Series de tiempo 'interpretacion JH'
 muertes_df = (pd.read_csv(dir_series + 'covid19_mex_muertes_nuevas.csv')
             .set_index('Fecha')['Nacional']
             .rename('Nuevas_JH'))
 muertes_df.index = pd.to_datetime(muertes_df.index)
 
+casos_df = (pd.read_csv(dir_series + 'covid19_mex_casos_nuevos.csv')
+            .set_index('Fecha')['Nacional']
+            .rename('Nuevos_JH'))
+casos_df.index = pd.to_datetime(casos_df.index)
+
+## Series de tiempo 'datos abiertos'
 muertes_abiertos_df = (pd.read_csv(dir_series_abiertos + 'covid19_mex_muertes.csv')
            .set_index('Fecha')['Nacional']
            .rename('Nuevas_abiertos'))
 muertes_abiertos_df.index = pd.to_datetime(muertes_abiertos_df.index)
 
-nuevas = pd.concat((muertes_df, muertes_abiertos_df), axis = 1).fillna(0).astype(int)
-acumuladas = nuevas.cumsum()
+casos_abiertos_df = (pd.read_csv(dir_series_abiertos + 'covid19_mex_confirmados.csv')
+           .set_index('Fecha')['Nacional']
+           .rename('Nuevos_abiertos'))
+casos_abiertos_df.index = pd.to_datetime(casos_abiertos_df.index)
+
+## Creamos dfs
+muertes_nuevas = pd.concat((muertes_df, muertes_abiertos_df), axis = 1).fillna(0).astype(int)
+muertes_acumuladas = muertes_nuevas.cumsum()
+
+casos_nuevos = pd.concat((casos_df, casos_abiertos_df), axis = 1).fillna(0).astype(int)
+casos_acumulados = casos_nuevos.cumsum()
 
 # cutoff = '2020-02-28'
 
@@ -28,5 +44,8 @@ acumuladas = nuevas.cumsum()
 # Escribimos archivos
 # .loc[cutoff:, :]
 
-nuevas.to_csv(dir_save + 'comparativo_muertes_nuevas.csv')
-acumuladas.to_csv(dir_save + 'comparativo_muertes_acumuladas.csv')
+muertes_nuevas.to_csv(dir_save + 'comparativo_muertes_nuevas.csv')
+muertes_acumuladas.to_csv(dir_save + 'comparativo_muertes_acumuladas.csv')
+
+casos_nuevos.to_csv(dir_save + 'comparativo_casos_nuevos.csv')
+casos_acumulados.to_csv(dir_save + 'comparativo_casos_acumulados.csv')
