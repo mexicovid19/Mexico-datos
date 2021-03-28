3d0f0f61e208aa18c80803280adc3d2c030e95c5
Rodrigo L.C <rodrigolealc@ciencias.unam.mx>
2021-03-28 20:41:55 +0100

    Updated code to deal with antigen tests; timeseries file is updated again

--- codigo/parsers.py
+++ codigo/parsers.py
@@ -16,7 +16,8 @@ def confirmados_diarios_por_estado(datos, entidades):
 
     [1]: https://www.gob.mx/salud/documentos/datos-abiertos-152127
     """
-    series = (datos[datos['CLASIFICACION_FINAL'].isin([1, 2, 3])]
+    idx = datos['CLASIFICACION_FINAL'].isin([1, 2, 3])
+    series = (datos.loc[idx]
               .groupby(['ENTIDAD_UM', 'FECHA_INGRESO'])
               .count()['ORIGEN'])
     return get_formato_series(series, entidades)
@@ -36,7 +37,8 @@ def negativos_diarios_por_estado(datos, entidades):
     [1]: https://www.gob.mx/salud/documentos/datos-abiertos-152127
 
     """
-    series = (datos[datos['CLASIFICACION_FINAL'] == 7]
+    idx = (datos['CLASIFICACION_FINAL'] == 7)
+    series = (datos.loc[idx]
               .groupby(['ENTIDAD_UM', 'FECHA_INGRESO'])
               .count()['ORIGEN'])
     return get_formato_series(series, entidades)
@@ -56,30 +58,32 @@ def sospechosos_diarios_por_estado(datos, entidades):
     [1]: https://www.gob.mx/salud/documentos/datos-abiertos-152127
 
     """
-    series = (datos[datos['CLASIFICACION_FINAL'].isin([3, 4, 5])]
+    idx = datos['CLASIFICACION_FINAL'].isin([3, 4, 5])
+    series = (datos.loc[idx]
               .groupby(['ENTIDAD_UM', 'FECHA_INGRESO'])
               .count()['ORIGEN'])
     return get_formato_series(series, entidades)
 
 
-# def pruebas_totales_diarias_por_estado(datos, entidades):
-#     """
-#     Calcula el número total de pruebas realizadas por fecha y por estado.
-# 
-#     Input:
-#     - datos: datos abiertos de COVID-19 en México disponibles en [1].
-# 
-#     Output:
-#     - series: Serie de tiempo de nuevas pruebas totales por dia para cada
-#         entidad federativa en México.
-# 
-#     [1]: https://www.gob.mx/salud/documentos/datos-abiertos-152127
-# 
-#     """
-#     series = (datos
-#               .groupby(['ENTIDAD_UM', 'FECHA_INGRESO'])
-#               .count()['ORIGEN'])
-#     return get_formato_series(series, entidades)
+def pruebas_totales_diarias_por_estado(datos, entidades):
+    """
+    Calcula el número total de pruebas realizadas por fecha y por estado.
+
+    Input:
+    - datos: datos abiertos de COVID-19 en México disponibles en [1].
+
+    Output:
+    - series: Serie de tiempo de nuevas pruebas totales por dia para cada
+        entidad federativa en México.
+
+    [1]: https://www.gob.mx/salud/documentos/datos-abiertos-152127
+
+    """
+    idx = (datos['TOMA_MUESTRA_LAB'] == 1) | (datos['TOMA_MUESTRA_ANTIGENO'] == 1)
+    series = (datos.loc[idx]
+              .groupby(['ENTIDAD_UM', 'FECHA_INGRESO'])
+              .count()['ORIGEN'])
+    return get_formato_series(series, entidades)
 
 
 def defunciones_diarias_por_estado(datos, entidades):
@@ -97,7 +101,7 @@ def defunciones_diarias_por_estado(datos, entidades):
 
     """
     idx = (datos['CLASIFICACION_FINAL'].isin([1, 2, 3])) & (datos['FECHA_DEF'] != '9999-99-99')
-    series = (datos[idx]
+    series = (datos.loc[idx]
               .groupby(['ENTIDAD_UM', 'FECHA_DEF'])
               .count()['ORIGEN'])
     return get_formato_series(series, entidades)
@@ -119,7 +123,7 @@ def hospitalizados_diarios_por_estado(datos, entidades):
     """
     # esta serie incluye UCI + noUCI
     idx = (datos['CLASIFICACION_FINAL'].isin([1, 2, 3]) & (datos['TIPO_PACIENTE'] == 2))
-    series = (datos[idx]
+    series = (datos.loc[idx]
               .groupby(['ENTIDAD_UM', 'FECHA_INGRESO'])
               .count()['ORIGEN'])
     return get_formato_series(series, entidades)
@@ -140,7 +144,7 @@ def ambulatorios_diarios_por_estado(datos, entidades):
 
     """
     idx = (datos['CLASIFICACION_FINAL'].isin([1, 2, 3]) & (datos['TIPO_PACIENTE'] == 1))
-    series = (datos[idx]
+    series = (datos.loc[idx]
               .groupby(['ENTIDAD_UM', 'FECHA_INGRESO'])
               .count()['ORIGEN'])
     return get_formato_series(series, entidades)
@@ -161,7 +165,8 @@ def uci_diarios_por_estado(datos, entidades):
 
     """
     idx = (datos['CLASIFICACION_FINAL'].isin([1, 2, 3]) & (datos['UCI'] == 1))
-    series = (datos[idx]
+    series = (datos.loc[idx]
               .groupby(['ENTIDAD_UM', 'FECHA_INGRESO'])
               .count()['ORIGEN'])
     return get_formato_series(series, entidades)
+
