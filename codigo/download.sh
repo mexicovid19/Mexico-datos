1e8a8879fb021a86f79f6c57a2b0d775059daf75
Rodrigo L.C <rodrigolealc@ciencias.unam.mx>
2021-04-08 15:08:09 +0100

    Refactores bash scripts

--- codigo/download.sh
+++ codigo/download.sh
@@ -2,9 +2,9 @@
 
 set -euo pipefail
 
+
 URL="https://www.gob.mx/salud/documentos/datos-abiertos-152127"
-URL_ZIP="http://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos"
-ZIP_FILE="datos_abiertos_covid19.zip"
+URL_ZIP="http://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip"
 
 DATE_CMD='date -d yesterday'  # date -d funciona en linux (bash)
 # En MacOS utilizar date -v (descomentar línea de abajo)
@@ -14,47 +14,50 @@ DATE_CMD='date -d yesterday'  # date -d funciona en linux (bash)
 DATE_PATTERN="[Bb]ase de [Dd]atos.*$( $DATE_CMD +"%d/%m/%Y" )"
 
 # después de descargar se renombra el archivo usando la fecha 
-FILENAME="$( $DATE_CMD +"%Y%m%d" )"
+TARGET_BASENAME="$( $DATE_CMD +"%Y%m%d" )"
 
-# La linea abajo consigue el direcororio donde se encuentra el script sin
+# dentro del archivo zip, el archivo csv tiene el siguiente nombre
+CSV_FILENAME="$( $DATE_CMD +"%y%m%d" )COVID19MEXICO.csv"
+
+# La linea abajo consigue el directorio donde se encuentra el script sin
 # importar de donde se llame (no funciona si el último componente es un symlink)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR
-cd ..
+cd "$DIR"
 
 
-
-# Creamos directorio temporal
-TMP_DIR=$(mktemp -dp .)  # -p "$REPO_DIR"
-echo -e "Directorio temporal es $TMP_DIR\n"
-
-# Nos aseguramos de limpiar archivos temporales
-trap 'rm "$ZIP_FILE" && rm -rf "$TMP_DIR"' EXIT
-
-
-# Descarga el archivo zip
 if curl -sSL "$URL" | tac | tac | grep -q "$DATE_PATTERN"; then
     echo -e "Pagina esta actualizada; inicia descarga\n"
-
-    curl -LO "$URL_ZIP/$ZIP_FILE" && unzip "$ZIP_FILE" -d "$TMP_DIR"
-    echo -e "\nTermino descarga y descompresion\n"
 else
     echo  -e "ERROR: pagina no esta actualizada"
     exit 1
 fi
 
 
-# Verifica que el archivo zip descomprimido correponda a un un solo archivo, renombra
-if [ $(ls -1 "$TMP_DIR" | wc -l) == "1" ]; then
-    DOWNLOAD_NAME=($TMP_DIR/*.csv)  # NB: globs are not expanded in quotes
-    echo -e "Corriendo script fix_latin\n"
+# Creamos directorio temporal
+TMP_DIR=$(mktemp -dp .)  # -p "$REPO_DIR
+echo -e "Directorio temporal es: $TMP_DIR\n"
 
-    # Se descartan columns
-    # awk -F, '{print $3","$5","$10","$11","$13","$36}' "$DATA_DIR/$FILENAME.csv" > "$DATA_DIR/$FILENAM    E.csv"
+# La descarga se hace dentro del directorio temporal
+cd "$TMP_DIR"
+
+# Borrar archivos temporales en caso de error
+trap 'rm -rf "$TMP_DIR"' EXIT
+
+
+# Descarga el archivo zip
+curl -L "$URL_ZIP" -o "$TARGET_BASENAME.zip"
+
+echo -e "\nDescomprimiendo"
+unzip "$TARGET_BASENAME.zip"
+
+if [ -f "$CSV_FILENAME" ]; then
+    # TODO: limpiar columnas por nombre
+    # awk -F, '{print $3","$5","$10","$11","$13","$36}' "$CSV_FILENAME" > "$TARGET_BASENAME.csv"
     # echo -e "Archivo csv reducido: solo las columnas necesarias fueron conservadas\n"
 
-    fix_latin "$DOWNLOAD_NAME" > "$TMP_DIR/$FILENAME.csv"
-    echo -e "Archivo csv renombrado: $FILENAME.csv (conversion fix_latin exitosa)\n"
+    echo -e "\nCorriendo script fix_latin sobre: $CSV_FILENAME"
+    fix_latin "$CSV_FILENAME" > "$TARGET_BASENAME.csv"
+    echo -e "Archivo corregido es: $TARGET_BASENAME.csv\n"
 
 else
     echo "ERROR: archivo csv no encontrado o hay ambiguedad"
@@ -63,15 +66,22 @@ fi
 
 
 # Conprimimos archivo csv y generamos zip
-if [ -f "$TMP_DIR/$FILENAME.csv" ]; then
-    zip  "$TMP_DIR/$FILENAME.zip" "$TMP_DIR/$FILENAME.csv"
-    mv "$TMP_DIR/$FILENAME.zip" .
-    echo -e "\nArchivo zip creado\n"
+if [ -f "$TARGET_BASENAME.csv" ]; then
+    echo -e "Comprimiendo (sobreescribe zip original)"
+    zip - "$TARGET_BASENAME.csv" > "$TARGET_BASENAME.zip"
+    # la redireccion a stdout sobreescribe zip original
+    
+    echo -e "\nMoviendo el archivo fuera de $TMP_DIR"
+    mv "$TARGET_BASENAME.zip" ..
+
+    # Borramos archivos temporales
+    cd ..
+    rm -rf "$TMP_DIR"
 else
     echo "ERROR: archivo csv no encontrado (paso de compresion)"
     exit 1
 fi
 
 
-echo "Termina script"
+echo -e "\nTermina script download.sh\n"
 
--- codigo/workflow.sh
+++ codigo/workflow.sh
@@ -1,46 +1,51 @@
 #!/bin/bash
 
-echo -e "\n$(date)"
-
-# Activa environment de conda
-CONDA_BASE="$(conda info --base)"
-CONDA_ENV="csv"
-source "$CONDA_BASE/etc/profile.d/conda.sh"
-conda activate  "$CONDA_ENV"
-
-
 set -euo pipefail
 
 
-# La linea abajo consigue el direcororio donde se encuentra el script sin
-# importar de donde se llame (no funcioa si el último componente es un symlink)
+echo -e "\n$(date)"
+
+# La linea abajo consigue el directorio donde se encuentra el script sin
+# importar de donde se llame (no funciona si el último componente es un symlink)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
+
 
 # Descarga la ultima base de datos
 /bin/bash ./download.sh
 # /bin/bash ./download_mirror.sh
 
-DATA_DIR=".."
-FILENAME="$(date -d "yesterday" +"%Y%m%d").zip"
-# si download_mirror, el nombre es el de abajo
-# FILENAME="datos_abiertos_$(date -d "yesterday" +"%Y%m%d").zip"
 
-if [ -f  $DATA_DIR/$FILENAME ]; then
-    echo -e "\nDatos bajados, inicia script Python\n"
-    python -u process_datos_abiertos.py $DATA_DIR/$FILENAME; echo
+# Activa el ambiente virtual
+source "env/bin/activate"
+trap 'deactivate' EXIT
+
+DATE_CMD='date -d yesterday'  # date -d funciona en linux (bash)
+# En MacOS utilizar date -v (descomentar línea de abajo)
+# DATE_CMD='date -v -1d'
+
+FILENAME="$( $DATE_CMD +"%Y%m%d" ).zip"
+# si download_mirror, el nombre es el de abajo
+# FILENAME="datos_abiertos_$FILENAME"
+
+
+if [ -f  "$FILENAME" ]; then
+    echo -e "\nBase de datos encontrada, inician scripts de Python\n"
+    python -u process_datos_abiertos.py "$FILENAME"; echo
     python update_tests.py
     python update_deceased.py
-    python update_pyramids.py $DATA_DIR/$FILENAME
+    python update_pyramids.py "$FILENAME"
 
-    # rm -rf $DATA_DIR  # no longer needed, cleanup is done in download.sh
-    git add ../datos ; git add ../datos_abiertos
+    git add ../datos ../datos_abiertos
     git commit -m "Automatic update"
     git push  #-f fork
 else
     echo "Archivo no encontrado"
 fi
 
+rm "$FILENAME"
+deactivate
 
-# fin del script
+
+echo -e "\nTermina script workflow.sh\n"
 
